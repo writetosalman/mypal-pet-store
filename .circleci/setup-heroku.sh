55a5032a90d7b707765f09be7134e9cfddb4c785
Salman A <write.to.salman@gmail.com>
2017-10-21 22:32:33 +1100

    Edited config.yml to push code to heroku

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -1,4 +1,8 @@
-wget -qO- https://cli-assets.heroku.com/install-ubuntu.sh | sh
+git remote add heroku https://git.heroku.com/mypal-pet-store.git
+wget http://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
+mkdir -p /usr/local/lib /usr/local/bin
+tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
+ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
 
 cat > ~/.netrc << EOF
 login $HEROKU_LOGIN
